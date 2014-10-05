f55f9223418c4b10df18d489b6457efc3048e523
Tim Davis <tadavis406@gmail.com>
2014-10-04 22:53:55 -0400

    Added a few comments to clarify things regarding biglm.

--- bigLM.R
+++ bigLM.R
@@ -1,3 +1,7 @@
+# Seems that it's important to use bigglm.ffdf, and not biglm.  Type coercion
+# may take place with biglm, in the sense that the ffdf data may be converted to a 
+# conventional data frame.  This we don't want.  We want the data handled via chunks.
+#
 require(biglm)
 Sys.time() -> start
 model = bigglm.ffdf(Y ~ X1 + X2 + X3, fromPostgres_ff)
